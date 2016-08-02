93ebf98aead2a2ddad7cf49837c8574e6cbd38cc
Praveer Gupta <praveerg@inpraveerg.local>
2016-08-02 15:13:55 +0530

    Add circuit breaker logic

--- src/main/java/com/example/servicetwo/ServiceTwoClient.java
+++ src/main/java/com/example/servicetwo/ServiceTwoClient.java
@@ -1,5 +1,7 @@
 package com.example.servicetwo;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,15 @@ public class ServiceTwoClient {
         this.restTemplate = restTemplate;
     }
 
-
+    @HystrixCommand(fallbackMethod = "defaultResponse")
     public String retrieveFromServiceTwo() {
         ResponseEntity<ServiceTwoResponse> response = this.restTemplate
                 .getForEntity("http://service-two/greet", ServiceTwoResponse.class);
         return response.getBody().getHello();
+    }
+
+    public String defaultResponse() {
+        return "default";
     }
 
 }
